294dc8d706e2475d28cb9e1103ed6491fa68868c
Matheus Windows <matheus.fnds20@gmail.com>
2025-08-08 17:46:57 -0300

    add for readings

---
+++ p1_chapter04_for.py
@@ -0,0 +1,6 @@
+magicians = ["alice", "david", "carolina"]
+
+for magician in magicians: # for every magician in this list of magicians, print the magicians name
+    print(f"{magician.title()}, you did great! Well done!")
+    
+# try to use the singular version of plural list cats -> cat, dogs -> dog

4a7c10e9698f8ae211a5d06e09d9f305738c6759
Matheus Windows <matheus.fnds20@gmail.com>
2025-08-08 14:46:05 -0300

    add exercises

--- p1_chapter03_list.py
+++ p1_chapter03_list.py
@@ -3,3 +3,11 @@ names = ['matheus', 'leticia', 'ligia', 'ana']
 print(f"Bem vindo, {names[2].title()}")
 names[0] = "fabricio" # alterei o matheus para fabricio
 print(names)
+
+del names[-1]
+print(names)
+popped_names = names.pop() # posso usar um numero de index que quiser
+print(popped_names)
+print(names)
+names.append('Ligia')
+print(names)
---
+++ p1_chapter03_list_exe.py
@@ -0,0 +1,9 @@
+#  3-4. Guest List: If you could invite anyone, living or deceased, to dinner, who 
+# would you invite? Make a list that includes at least three people you’d like to 
+# invite to dinner. Then use your list to print a message to each person, inviting 
+# them to dinner.
+
+guest = ["grandma Iolanda", "Thomas Kuhn", "Vicente Mateus"]
+
+for x in guest:
+    print(f"Wellcome, to my dinner party {x}")

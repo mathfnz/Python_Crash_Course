00e4a69e9bbe2942fab56a72ce46a1dc38cb5612
mathfnz_macbook <matheus.fnds20@gmail.com>
2025-08-28 13:46:24 -0300

    chapter 01 and 02

---
+++ hellowor.py
@@ -0,0 +1,16 @@
+#%%
+hi = "Hello word!"
+type(hi)
+# %% 2.1
+name: str = "Olá, Matheus"
+print(name)
+# %% 2.2
+hi: str = "Olá, Matheus"
+print(hi)
+hi: str = "Matheus"
+print(hi)
+# %%
+print(hi.upper())
+# %%
+print(f"Hello, {hi}")
+# %%

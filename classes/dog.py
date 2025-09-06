bbb65f0600ed0b250dcb8ba42a7080865025ab97
mathfnz_macbook <matheus.fnds20@gmail.com>
2025-09-06 20:17:21 -0300

    add

---
+++ classes/dog.py
@@ -0,0 +1,14 @@
+class Dog:
+    """A dog model"""
+    def __init__(self, name, age):
+        """Initialize name and age attributes"""
+        self.name = name
+        self.age = age
+        
+    def sit(self):
+        """Simulate a dog sitting"""
+        print(f"{self.name} is now sitting")
+
+    def roll_over(self):
+        """Simulate rolling over"""
+        print(f"{self.name} rolled over")
